fb5e80a61d851758276b633bcd328c5dc422a4b0
LePeruvienn <koto91@free.fr>
2025-06-08 19:18:19 +0200

    Fix Player FPS controller issues

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -251,17 +251,25 @@ namespace StarterAssets
 			// normalise input direction
 			Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
-			// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 			// Rotate the player depending of the camera direction
-			_targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
-							  _mainCamera.transform.eulerAngles.y;
+			_targetRotation = _mainCamera.transform.eulerAngles.y;
 			float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
 				RotationSmoothTime);
 
 			// rotate to face input direction relative to camera position
 			transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
 
-			Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+			// Get camera direction horizontal and vertical direction in space
+			Vector3 cameraForward = _mainCamera.transform.forward;
+			Vector3 cameraRight = _mainCamera.transform.right;
+
+			// Reset vertical values to get only a 2D direction
+			cameraForward.y = 0f;
+			cameraRight.y = 0f;
+
+			// Compute target targetDirection depending of player's inputs
+			Vector3 targetDirection = cameraForward * inputDirection.z + cameraRight * inputDirection.x;
 
 			// move the player
 			_controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
